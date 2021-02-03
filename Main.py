53f12d54f0c9cf94ebf59ab9f917df40c5a57b61
KR0SIV <sarah@giddings.me>
2021-02-03 16:09:27 -0500

    Adding main kv file

--- Main.py
+++ Main.py
@@ -81,30 +81,22 @@ def queryserver(widget):
 
 
 class Pi25mch(MDApp):
-    #def __init__(**kwargs):
-        #self.theme_cls.theme_style = "Dark"
-   #     super().__init__(**kwargs)
     previous_date = ObjectProperty()
     title = "Pi25: Mobile Control Head"
 
     def build(self):
-        #main_widget = Builder.load_string(main_widget_kv)
         main_widget = Builder.load_file('kv/main.kv')
-        # self.theme_cls.theme_style = 'Dark'
-#        main_widget.ids.scanner_label.text = str(return_value['tag'])
         self.bottom_navigation_remove_mobile(main_widget)
-
         Thread(target=lambda: queryserver(main_widget)).start()
         return main_widget
 
     def bottom_navigation_remove_mobile(self, widget):
         # Removes some items from bottom-navigation demo when on mobile
-        if DEVICE_TYPE == 'mobile':
-            widget.ids.bottom_navigation_demo.remove_widget(widget.ids.bottom_navigation_desktop_2)
-        if DEVICE_TYPE == 'mobile' or DEVICE_TYPE == 'tablet':
-            widget.ids.bottom_navigation_demo.remove_widget(widget.ids.bottom_navigation_desktop_1)
-
-
+        #if DEVICE_TYPE == 'mobile':
+        #    widget.ids.bottom_navigation_demo.remove_widget(widget.ids.bottom_navigation_desktop_2)
+        #if DEVICE_TYPE == 'mobile' or DEVICE_TYPE == 'tablet':
+        #    widget.ids.bottom_navigation_demo.remove_widget(widget.ids.bottom_navigation_desktop_1)
+        pass
 
 
 if __name__ == '__main__':
